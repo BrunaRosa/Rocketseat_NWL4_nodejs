299adeb3dfb1f41b312f5e46a37f8422f6f9fdd7
BrunaRosa <brosa@bionexo.com>
2021-03-01 23:27:48 -0300

    Refactoring app server

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,12 @@
 import 'reflect-metadata'
 import express, { request, response } from 'express';
 import "./database";
+import { router } from './routes';
 
 const app = express();
 
-
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World - NWL04!" });
-});
-
-app.post("/", (request, response) => {
-    return response.json({ message: "Os dados foram salvos com sucesso!"});
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, ()  => console.log("Server is running!"));
 
