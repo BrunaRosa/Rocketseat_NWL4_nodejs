f8cb9feebc2309273165a2dacb667abf6b08c850
BrunaRosa <brosa@bionexo.com>
2021-03-03 22:20:28 -0300

    Add routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,23 @@
 import { Router } from 'express';
 import { UserController } from './controllers/UserController';
+import { SurveysController } from './controllers/SurveysController';
+// import { SendMailController } from './controllers/SendMailController';
+// import { AnswerController } from './controllers/AnswerControllers';
+// import { NpsController } from './controllers/NpsController';
 
 const router = Router();
 
 const userController = new UserController();
+const surveysController = new SurveysController();
+// const sendMailController = new SendMailController();
+// const answerController = new AnswerController();
+// const npsController = new NpsController();
 
-router.post("/users", userController.create);
+router.post('/users', userController.create);
+router.post('/surveys', surveysController.create);
+// router.post('/sendMail', sendMailController.execute);
+router.get('/surveys', surveysController.show);
+// router.get('/answers/:value', answerController.execute);
+// router.get('/nps/:survey_id', npsController.execute);
 
 export { router };
